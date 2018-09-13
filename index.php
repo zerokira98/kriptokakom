1089fab6814aa804e1e8fe3598acd1d476ca84df
zerokira98 <31804242+zerokira98@users.noreply.github.com>
2018-09-13 13:54:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Tugas kakom</title>
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
     <style media="screen">
       ul{
